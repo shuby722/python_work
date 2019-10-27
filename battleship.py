c8007e8ba9f07aec3a0f8d46524d22104ecfc504
Brad <shuby722@gmail.com>
2019-10-27 13:17:44 -0400

    Making notes for more work possible later on

--- battleship.py
+++ battleship.py
@@ -36,7 +36,7 @@ for turn in range(4):
     if guess_row == ship_row and guess_col == ship_col:
       print "Congratulations! You sunk my battleship!"
       break
-      
+
     else:
       if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
         print "Oops, that's not even in the ocean."
@@ -53,3 +53,22 @@ for turn in range(4):
 
       print "Turn", turn + 1
       print_board(board)
+
+
+
+
+
+
+
+
+# You can also add on to your Battleship! program to make it more complex and fun to play. Here are some ideas for enhancements—maybe you can think of some more!
+
+# Make multiple battleships: you’ll need to be careful because you need to make sure that you don’t place battleships on top of each other on the game board. 
+# You’ll also want to make sure that you balance the size of the board with the number of ships so the game is still challenging and fun to play.
+
+# Make battleships of different sizes: this is trickier than it sounds. All the parts of the battleship need to be vertically or horizontally touching 
+# and you’ll need to make sure you don’t accidentally place part of a ship off the side of the board.
+
+# Make your game a two-player game.
+
+# Use functions to allow your game to have more features like rematches, statistics and more!
