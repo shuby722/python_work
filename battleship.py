10ff4aa0e73492f8cda54fe4fd5abfc142d86db1
Brad <bradly.shubitowski1@ge.com>
2019-12-04 15:33:27 -0500

    adding close but no cigar, need case when you are one off

--- battleship.py
+++ battleship.py
@@ -15,10 +15,10 @@ print_board(board)
 
 #Set where the battleship will go
 def random_row(board):
-  return randint(0, len(board) - 1)
+  return randint(1, len(board))
 
 def random_col(board):
-  return randint(0, len(board[0]) - 1)
+  return randint(1, len(board[0]))
 
 ship_row = random_row(board)
 ship_col = random_col(board)
@@ -28,47 +28,39 @@ print ship_row
 print ship_col
 
 
+
 #Makes game 4 turns long unless you win
-for turn in range(4):
-    guess_row = int(raw_input("Guess Row: "))
-    guess_col = int(raw_input("Guess Col: "))
+def run_game():
+  for turn in range(4):
+      print "Turn", turn
+      guess_row = int(raw_input("Guess Row: "))-1
+      guess_col = int(raw_input("Guess Col: "))-1
 
-    if guess_row == ship_row and guess_col == ship_col:
-      print "Congratulations! You sunk my battleship!"
-      break
-
-    else:
-      if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-        print "Oops, that's not even in the ocean."
-
-      elif(board[guess_row][guess_col] == "X"):
-        print "You guessed that one already."
-
+      if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sunk my battleship!"
+        break
+    
       else:
-        print "You missed my battleship!"
-        board[guess_row][guess_col] = "X"
+        if (guess_row < 1 or guess_row > 5) or (guess_col < 1 or guess_col > 5):
+          print "Oops, that's not even in the ocean."
 
-      if turn == 3:
-        print "Game Over"
+        elif(board[guess_row][guess_col] == "X"):
+          print "You guessed that one already."
 
-      print "Turn", turn + 1
-      print_board(board)
+        elif((guess_row - ship_row == 1 or guess_row - ship_row == -1) and (guess_col - ship_col == 1 or guess_col - ship_col == -1)):
+      	  print "Close, but no cigar!"
+      	  print "You missed my battleship!"
+          board[guess_row][guess_col] = "X"
 
+        else:
+          print "You missed my battleship!"
+          board[guess_row][guess_col] = "X"
 
+        if turn == 3:
+          print "Game Over"
+          print "Ship was at", ship_row, ship_col
 
+        print "Turn", turn + 1
+        print_board(board)
 
-
-
-
-
-# You can also add on to your Battleship! program to make it more complex and fun to play. Here are some ideas for enhancements—maybe you can think of some more!
-
-# Make multiple battleships: you’ll need to be careful because you need to make sure that you don’t place battleships on top of each other on the game board. 
-# You’ll also want to make sure that you balance the size of the board with the number of ships so the game is still challenging and fun to play.
-
-# Make battleships of different sizes: this is trickier than it sounds. All the parts of the battleship need to be vertically or horizontally touching 
-# and you’ll need to make sure you don’t accidentally place part of a ship off the side of the board.
-
-# Make your game a two-player game.
-
-# Use functions to allow your game to have more features like rematches, statistics and more!
+run_game()
